a8f0a1c10d81433620d3ff1706936cfdd6845ca7
Maxwell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-05-02 18:01:03 -0700

    javascript

---
+++ Develop/index.js
@@ -0,0 +1,109 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Add a short description for your prject.",
+  },
+ 
+  {
+    type: "input",
+    name: "deploymentLink",
+    message: "Please provide a URL where a user can access your deployed application."
+  },
+  {
+    type: "input",
+    name: "instalation",
+    message: "Please provide steps for instalation."
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "badges",
+    message: "Provide badge link"
+  },
+  {
+    type: "input",
+    name: "require",
+    message: "List any project dependencies here.",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "What are some features about your project?",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: " Provide instructions and examples for use. Include screenshots as needed. To add a screen shot, create an assets/images folder in your repo and upload your screenshot to it. Then using the relative file path, add it to your README using the following syntax: ![alt text] (assets/images/screenshot.png)"
+      
+  },
+  {
+    type: "input",
+    name: "credits",
+    message: "List your collaborators, if any, with links to their GitHub profiles."
+
+
+  },
+  {
+    type: "input",
+    name: "githubUser",
+    message: "What is your Github Username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address.",
+  },
+  {
+    type: "input",
+    name: "contribute",
+    message: "Explain how to contribute to your project.",
+    
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Provide a walkthrough of required tests if applicable.",
+  },
+];
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+
+
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log("Now creating your README.md...");
+    //FIX File path
+    writeToFile("./dist/README.md", generateMarkdown({ ...answers }));
+  });
+}
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,101 @@
+// Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  
+    if (license !== "none") {
+      return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    }
+    return "";
+  
+}
+
+// Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+        return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+        return 'https://www.apache.org/licenses/LICENSE-2.0';
+    case 'GPL 3.0':
+        return 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+    // Add more cases for other licenses as needed
+    default:
+        return '';
+}
+ 
+
+}
+
+// Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  switch (license) {
+    case 'MIT':
+        return `
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+        `;
+    case 'Apache 2.0':
+        return `
+## License
+
+This project is licensed under the [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0).
+        `;
+    case 'GPL 3.0':
+        return `
+## License
+
+This project is licensed under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0.en.html).
+        `;
+    // Add more cases for other licenses as needed
+    default:
+        return '';
+}
+}
+ 
+
+// Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title} 
+  ## Description
+  ${data.description}
+  ## Deployed Application URL
+  ${data.deploymentLink}
+  ## Table of Contents
+  * [Instalation] (#instalation)
+  * [Languages & Dependencies](#languagesanddependencies)
+  * [Usage](#Usage)
+  * [Credits] (#Credits)
+  * [License] (#Licnese)
+  * [Badges] (#Badges)
+  * [Features](#features)
+  * [How to contribute](#contribute)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ## Instalation
+  ${data.instalation}
+  ## Languages & Dependencies
+  ${data.require}
+  ## Usage
+  ${data.usage}
+  ## Credits
+  ${data.credits}
+  ## License
+  ${renderLicenseBadge(data.license)}
+  ## Badges
+  ${data.badges}
+  ## Features
+  ${data.features}
+  ## How to contribute
+  ${data.contribute}
+  ## Tests
+  ${data.test}
+  ## Questions
+  Please email your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.githubUser}](https://github.com/${data.githubUser}).
+  `;
+  }
+
+
+module.exports = generateMarkdown;
